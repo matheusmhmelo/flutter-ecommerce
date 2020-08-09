aafc6bb82503b1ca970bd8117f347b0807fd070e
matheusmelo <ma_hrm@hotmail.com>
2020-08-08 21:59:00 -0300

    initial commit with base screend and login

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/user_manager.dart';
+import 'package:loja_virtual/screens/base/base_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  runApp(MyApp());
+
+  Firestore.instance
+      .collection('pedidos').snapshots().listen((snapshot) {
+        for(DocumentSnapshot document in snapshot.documents) {
+          print(document.data);
+        }
+  });
+
+
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => UserManager(),
+      child: MaterialApp(
+        title: 'Loja do Matheus',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: const Color.fromARGB(255, 4, 125, 141),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 4, 125, 141),
+          appBarTheme: const AppBarTheme(
+            elevation: 0
+          ),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: BaseScreen(),
+      ),
+    );
+  }
+}
