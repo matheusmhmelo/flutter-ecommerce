e2830c9cfb67661d11ad9be4e9ad131c65786cb7
matheusmelo <ma_hrm@hotmail.com>
2020-08-11 21:48:41 -0300

    finished products screen

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/item_size.dart';
 
-class Product {
+class Product extends ChangeNotifier {
 
   Product.fromDocument(DocumentSnapshot document){
     id = document.documentID;
@@ -8,11 +10,38 @@ class Product {
     description = document["description"] as String;
 
     images = List<String>.from(document.data["images"] as List<dynamic>);
+
+    sizes = (document.data["sizes"] as List<dynamic> ?? []).map(
+      (s) => ItemSize.fromMap(s as Map<String, dynamic>)
+    ).toList();
   }
 
   String id;
   String name;
   String description;
   List<String> images;
+  List<ItemSize> sizes;
+
+  ItemSize _selectedSize;
+
+  ItemSize get selectedSize => _selectedSize;
+
+  set selectedSize(ItemSize value){
+    _selectedSize = value;
+    notifyListeners();
+  }
+
+  int get totalStock {
+    int stock = 0;
+    for(final size in sizes){
+      stock += size.stock;
+    }
+
+    return stock;
+  }
+
+  bool get hasStock {
+    return totalStock > 0;
+  }
 
 }
