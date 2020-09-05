104212b3d73cc67b36c0613a53d3e8875c50eae9
matheusmelo <ma_hrm@hotmail.com>
2020-09-05 19:52:15 -0300

    Credit card page

--- lib/screens/checkout/checkout_screen.dart
+++ lib/screens/checkout/checkout_screen.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/common/price_card.dart';
 import 'package:loja_virtual/models/cart_manager.dart';
 import 'package:loja_virtual/models/checkout_manager.dart';
-import 'package:loja_virtual/models/page_manager.dart';
 import 'package:provider/provider.dart';
+
+import 'components/credit_card_widget.dart';
 
 class CheckoutScreen extends StatelessWidget {
 
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -22,54 +24,66 @@ class CheckoutScreen extends StatelessWidget {
           title: const Text('Pagamento'),
           centerTitle: true,
         ),
-        body: Consumer<CheckoutManager>(
-          builder: (_, checkoutManager, __ ){
-            if(checkoutManager.loading) {
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation(Colors.white),
-                    ),
-                    SizedBox(height: 16,),
-                    Text(
-                      "Processando seu pagamento...",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 16
+        body: GestureDetector(
+          onTap: (){
+            FocusScope.of(context).unfocus();
+          },
+          child: Consumer<CheckoutManager>(
+            builder: (_, checkoutManager, __ ){
+              if(checkoutManager.loading) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation(Colors.white),
                       ),
+                      SizedBox(height: 16,),
+                      Text(
+                        "Processando seu pagamento...",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 16
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }
+              return Form(
+                key: formKey,
+                child: ListView(
+                  children: [
+                    CreditCardWidget(),
+                    PriceCard(
+                      buttonText: "Finalizar Pedido",
+                      onPressed: (){
+                        if(formKey.currentState.validate()) {
+                          print("enviado");
+//                        checkoutManager.checkout(
+//                            onStockFail: (e) {
+//                              Navigator.of(context).popUntil(
+//                                      (route) => route.settings.name == '/cart'
+//                              );
+//                            },
+//                            onSuccess: (order) {
+//                              Navigator.of(context).popUntil(
+//                                      (route) => route.settings.name == '/');
+//                              Navigator.of(context).pushNamed(
+//                                  "/confirmation",
+//                                  arguments: order
+//                              );
+//                            }
+//                        );
+                        }
+                      },
                     )
                   ],
                 ),
               );
-            }
-            return ListView(
-              children: [
-                PriceCard(
-                  buttonText: "Finalizar Pedido",
-                  onPressed: (){
-                    checkoutManager.checkout(
-                      onStockFail: (e) {
-                        Navigator.of(context).popUntil(
-                          (route) => route.settings.name == '/cart'
-                        );
-                      },
-                      onSuccess: (order){
-                        Navigator.of(context).popUntil(
-                          (route) => route.settings.name == '/');
-                        Navigator.of(context).pushNamed(
-                          "/confirmation",
-                          arguments: order
-                        );
-                      }
-                    );
-                  },
-                )
-              ],
-            );
-          },
+            },
+          ),
         )
       ),
     );
---
+++ lib/screens/checkout/components/card_front.dart
@@ -0,0 +1,103 @@
+import 'package:brasil_fields/brasil_fields.dart';
+import 'package:credit_card_type_detector/credit_card_type_detector.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:loja_virtual/screens/checkout/components/card_text_field.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+
+class CardFront extends StatelessWidget {
+
+  CardFront({this.numberFocus, this.nameFocus, this.dateFocus, this.finished});
+
+  final MaskTextInputFormatter dateFormatter = MaskTextInputFormatter(
+    mask: "!#/####", filter: {'#': RegExp('[0-9]'), '!': RegExp('[0-1]')}
+  );
+
+  final VoidCallback finished;
+
+  final FocusNode numberFocus;
+  final FocusNode dateFocus;
+  final FocusNode nameFocus;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 16,
+      child: Container(
+        height: 200,
+        color: const Color(0xFF1B4B52),
+        padding: const EdgeInsets.all(24),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  CardTextField(
+                    title: "Número",
+                    hint: "0000 0000 0000 0000",
+                    textInputType: TextInputType.number,
+                    bold: true,
+                    inputFormatters: [
+                      WhitelistingTextInputFormatter.digitsOnly,
+                      CartaoBancarioInputFormatter()
+                    ],
+                    validator: (number){
+                      if(number.length != 19) {
+                        return "Inválido";
+                      } else if (detectCCType(number) == CreditCardType.unknown){
+                        return "Inválido";
+                      }
+                      return null;
+                    },
+                    onSubmited: (_){
+                      dateFocus.requestFocus();
+                    },
+                    focusNode: numberFocus,
+                  ),
+                  CardTextField(
+                    title: "Validade",
+                    hint: "11/2021",
+                    textInputType: TextInputType.number,
+                    inputFormatters: [
+                      dateFormatter
+                    ],
+                    validator: (date){
+                      if(date.length != 7){
+                        return "Inválido";
+                      }
+                      return null;
+                    },
+                    onSubmited: (_){
+                      nameFocus.requestFocus();
+                    },
+                    focusNode: dateFocus,
+                  ),
+                  CardTextField(
+                    title: "Titular",
+                    hint: "Digite o nome exibido no cartão",
+                    textInputType: TextInputType.text,
+                    bold: true,
+                    validator: (name){
+                      if(name.isEmpty) {
+                        return 'Inválido';
+                      }
+                      return null;
+                    },
+                    onSubmited: (_){
+                      finished();
+                    },
+                    focusNode: nameFocus,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
